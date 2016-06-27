5a6c7281b7e1569b1bcad1dac4559c50ca3322ad
stopal <stopal@rapid7.com>
2016-06-27 16:26:10 +0100

    Initial commit

---
+++ tests/examples/misc_examples.py
@@ -0,0 +1,29 @@
+TEST_OWNER_APIKEY = 'test_owner_apikey'
+TEST_OWNER_APIKEY_ID = 'test_owner_apikey_id'
+TEST_ACCOUNT_RESOURCE_ID = 'test_account_resource_id'
+
+TEST_USER_ID = "test_user_id"
+DUMMY_USER_CONTENT = '{"first_name": "", ' \
+                     '"last_name": "", ' \
+                     '"login_name": "", ' \
+                     '"email": "", ' \
+                     '"id": ""}'
+
+TEST_APIKEY_WITH_VALID_LENGTH = '123456789012345678901234567890123456'
+TEST_APIKEY_WITH_INVALID_LENGTH = '123456789012345678901234567890123456_invalid'
+
+TEST_QUERY = 'where(logID) calculate(count) sort(desc) limit(3)'
+
+TEST_LOG_KEY = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+TEST_LOG_GROUP = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\n' \
+                 'YYYYYYYY-YYYY-YYYY-YYYY-YYYYYYYYYYYY'
+
+TIME_FROM = '1465370400'
+TIME_TO = '1465370500'
+
+DATE_FROM = '2016-05-18 11:04:00'
+DATE_TO = '2016-05-18 11:09:59'
+
+MOCK_QUERYAPI_URL = 'http://mydummylink.com/query/'
+
+MOCK_USERAPI_URL = 'http://mydummylink.com/user'
---
+++ tests/test_apiutils.py
@@ -0,0 +1,165 @@
+import ConfigParser
+import hmac
+
+from mock import patch
+
+from lecli import apiutils
+from examples import misc_examples as misc_ex
+
+
+def test_gensignature():
+    with patch.object(hmac.HMAC, 'digest', return_value='digest_output'):
+        api_key = misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+        date = 'date'
+        content_type = 'content_type'
+        request_method = 'method'
+        request_body = 'body'
+        query_path = 'path'
+
+        signature = apiutils.gensignature(api_key, date, content_type, request_method, request_body, query_path)
+
+        assert signature == 'digest_output'
+
+
+@patch('lecli.apiutils.get_ro_apikey')
+def test_generate_headers_ro(mocked_ro_apikey):
+    mocked_ro_apikey.return_value = misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+    headers = apiutils.generate_headers(api_key_type='ro')
+
+    assert "x-api-key" in headers
+    assert headers["x-api-key"] == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+@patch('lecli.apiutils.get_rw_apikey')
+def test_generate_header_rw(mocked_rw_apikey):
+    mocked_rw_apikey.return_value = misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+    headers = apiutils.generate_headers(api_key_type='rw')
+
+    assert 'x-api-key' in headers
+    assert headers['x-api-key'] == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+@patch('lecli.apiutils.get_owner_apikey')
+@patch('lecli.apiutils.get_owner_apikey_id')
+def test_generate_header_owner(mocked_owner_apikey, mocked_owner_apikey_id):
+    mocked_owner_apikey.return_value = misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+    mocked_owner_apikey_id.return_value = misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+    headers = apiutils.generate_headers(api_key_type='owner', body='', method="GET", action="action")
+
+    assert 'Date' in headers
+    assert 'authorization-api-key' in headers
+    assert misc_ex.TEST_APIKEY_WITH_VALID_LENGTH in headers['authorization-api-key']
+
+
+def test_get_valid_ro_apikey():
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_VALID_LENGTH):
+        ro_api_key = apiutils.get_ro_apikey()
+
+        assert ro_api_key == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+def test_get_invalid_ro_apikey(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH):
+        ro_api_key = apiutils.get_ro_apikey()
+        out, err = capsys.readouterr()
+
+        assert ro_api_key == misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH
+        assert 'Error: Read-only API Key not of correct length\n' == out
+
+
+def test_get_valid_rw_apikey():
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_VALID_LENGTH):
+        rw_api_key = apiutils.get_rw_apikey()
+
+        assert rw_api_key == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+def test_get_invalid_rw_apikey(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH):
+        rw_api_key = apiutils.get_rw_apikey()
+        out, err = capsys.readouterr()
+
+        assert rw_api_key == misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH
+        assert 'Error: Read/Write API Key not of correct length\n' == out
+
+
+def test_get_valid_owner_apikey():
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_VALID_LENGTH):
+        owner_api_key = apiutils.get_owner_apikey()
+
+        assert owner_api_key == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+def test_get_invalid_owner_apikey(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH):
+        owner_api_key = apiutils.get_owner_apikey()
+        out, err = capsys.readouterr()
+
+        assert owner_api_key == misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH
+        assert 'Error: Owner API Key not of correct length\n' == out
+
+
+def test_get_valid_owner_apikey_id():
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_VALID_LENGTH):
+        owner_api_key_id = apiutils.get_owner_apikey_id()
+
+        assert owner_api_key_id == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+def test_get_invalid_owner_apikey_id(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH):
+        owner_api_key_id = apiutils.get_owner_apikey_id()
+        out, err = capsys.readouterr()
+
+        assert owner_api_key_id == misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH
+        assert 'Error: Owner API Key ID not of correct length\n' == out
+
+
+def test_get_valid_account_resource_id():
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_VALID_LENGTH):
+        account_resource_id = apiutils.get_account_resource_id()
+
+        assert account_resource_id == misc_ex.TEST_APIKEY_WITH_VALID_LENGTH
+
+
+def test_get_invalid_account_resource_id(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'get', return_value=misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH):
+        account_resource_id = apiutils.get_account_resource_id()
+        out, err = capsys.readouterr()
+
+        assert account_resource_id == misc_ex.TEST_APIKEY_WITH_INVALID_LENGTH
+        assert 'Error: Account Resource ID not of correct length\n' == out
+
+
+def test_get_valid_named_logkey():
+    with patch.object(ConfigParser.ConfigParser, 'items', return_value=[('test-logkey-nick', misc_ex.TEST_LOG_KEY)]):
+        logkey = apiutils.get_named_logkey('test-logkey-nick')
+        assert logkey == (misc_ex.TEST_LOG_KEY,)
+
+
+def test_get_invalid_named_logkey(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'items', return_value=[('test-logkey-nick', misc_ex.TEST_LOG_KEY)]):
+        nick_to_query = 'test-logkey-nick_invalid'
+        logkey = apiutils.get_named_logkey(nick_to_query)
+        out, err = capsys.readouterr()
+        assert logkey is None
+        assert 'Error: No nickname with name ' + '\'' + nick_to_query + '\'\n' == out
+
+
+def test_get_valid_named_group_key():
+    with patch.object(ConfigParser.ConfigParser, 'items',
+                      return_value=[('test-log-group-nick', misc_ex.TEST_LOG_GROUP)]):
+        logkeys = apiutils.get_named_logkey_group('test-log-group-nick')
+        assert logkeys == filter(None, str(misc_ex.TEST_LOG_GROUP).splitlines())
+
+
+def test_get_invalid_named_group_key(capsys):
+    with patch.object(ConfigParser.ConfigParser, 'items',
+                      return_value=[('test-log-group-nick', ["test-log-key1", "test-log-key2"])]):
+        nick_to_query = 'test-log-group-nick-invalid'
+        logkeys = apiutils.get_named_logkey_group(nick_to_query)
+        out, err = capsys.readouterr()
+        assert logkeys is None
+        assert 'Error: No group with name ' + '\'' + nick_to_query + '\'\n' == out
