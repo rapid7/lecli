1357f2a21b0c6293ecbe2f4da7b4523ea5ab1745
Emma Louise Lynch <ellynch@rapid7.com>
2017-01-26 11:36:49 +0000

    Updated documentation and renamed some commands

--- lecli/cli.py
+++ lecli/cli.py
@@ -57,25 +57,25 @@ if __name__ == '__main__':
 cli.add_command(query_commands.query)
 
 get.add_command(query_commands.get_events, "events")
-get.add_command(query_commands.get_recent_events, "recent_events")
-get.add_command(saved_query_commands.get_saved_query, "saved_query")
-get.add_command(saved_query_commands.get_saved_queries, "saved_queries")
+get.add_command(query_commands.get_recent_events, "recentevents")
+get.add_command(saved_query_commands.get_saved_query, "savedquery")
+get.add_command(saved_query_commands.get_saved_queries, "savedqueries")
 get.add_command(team_commands.get_team, "team")
 get.add_command(team_commands.get_teams, "teams")
 get.add_command(usage_commands.get_usage, "usage")
 get.add_command(user_commands.get_owner, "owner")
 get.add_command(user_commands.get_users, "users")
 
-create.add_command(saved_query_commands.create_saved_query, "saved_query")
+create.add_command(saved_query_commands.create_saved_query, "savedquery")
 create.add_command(team_commands.create_team, "team")
 create.add_command(user_commands.create_user, "user")
 
-update.add_command(saved_query_commands.update_saved_query, "saved_query")
+update.add_command(saved_query_commands.update_saved_query, "savedquery")
 update.add_command(team_commands.addusertoteam, "team_with_user")
 
 rename.add_command(team_commands.rename_team, "team")
 
-delete.add_command(saved_query_commands.delete_saved_query, "saved_query")
+delete.add_command(saved_query_commands.delete_saved_query, "savedquery")
 delete.add_command(team_commands.delete_team, "team")
 delete.add_command(team_commands.deleteuserfromteam, "user_from_team")
 delete.add_command(user_commands.delete_user, "user")
